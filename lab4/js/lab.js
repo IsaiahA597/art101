ee1b941c0d81107cae330408850f35a503b1b624
IsaiahA597 <iarreol1@ucsc.edu>
2024-04-16 20:47:02 -0700

    added Pseudocode code to js and index.html

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,32 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Connect-4
+// An implementation of the game
+// Isaiah Arreola
+// April 2024
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// create the grid board before each turn 
+  // loop through each row 
+    // loop through each space
+      // print a red circle or a yellow circle 
+      // print a line between 
+    // print a newline at the end
+    // print a horizontal line between
+// a player chooses a color and it’s sets of pieces
+  // computer gets the color that isn’t picked by the player
+// ask the player for their move 
+  // if the move is valid
+    // execute the move on the board
+    // check for any captures
+    // check for a win condition
+    // switch to the computer player
+  // if the move is not valid
+    // ask the player to make another move
+// accept user input
+// validate user move
+// evaluate possible moves
+// choose computer move
+  // if the move is valid:
+    // execute the move on the board
+    // check for any captures
+    // check for a win condition
+    // switch to the player
+// test of win or draw condition
