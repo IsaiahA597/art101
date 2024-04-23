5061abbddcffdfc74ab20e00e5574714380a49b1
IsaiahA597 <iarreol1@ucsc.edu>
2024-04-22 17:19:32 -0700

    .

---
+++ lab5/js/lab5.js
@@ -0,0 +1,23 @@
+/*
+ * Author: Isaiah Arreola
+ * Created: 21 April
+ * License: Public Domain
+ */
+
+// Define Variables
+make = "Gillig";
+model = "Santa-Cruz-Metro";
+color = "White";
+year = 2021;
+ownit = "no";
+
+// calculate
+age = 2024 - year;
+
+// output
+document.writeln("Make:" + make + "<br>");
+document.writeln("Model:" + model + "<br>");
+document.writeln("Color:" + color + "<br>");
+document.writeln("Year:" + year + "<br>");
+document.writeln("Age:" + age + " years<br>");
+document.writeln("Ownit:" + ownit + "<br>");
