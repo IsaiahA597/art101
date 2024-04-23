ac1dc03776d83f36dba87838080966ac98268591
IsaiahA597 <iarreol1@ucsc.edu>
2024-04-22 20:33:58 -0700

    .

--- lab5/js/lab5.js
+++ lab5/js/lab5.js
@@ -9,15 +9,15 @@ make = "Gillig";
 model = "Santa-Cruz-Metro";
 color = "White";
 year = 2021;
-ownit = "no";
+ownit = "No";
 
 // calculate
 age = 2024 - year;
 
 // output
-document.writeln("Make:" + make + "<br>");
-document.writeln("Model:" + model + "<br>");
-document.writeln("Color:" + color + "<br>");
-document.writeln("Year:" + year + "<br>");
-document.writeln("Age:" + age + " years<br>");
-document.writeln("Ownit:" + ownit + "<br>");
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+document.writeln("Ownit: " + ownit + "<br>");
