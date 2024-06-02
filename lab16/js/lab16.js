65cf2221f2c546fb39d61d1722c5bffd48f37b0d
IsaiahA597 <iarreol1@ucsc.edu>
2024-06-02 14:08:19 -0700

    .

--- lab16/js/lab16.js
+++ lab16/js/lab16.js
@@ -1,51 +1,79 @@
-/*
-    Author: Isaiah Arreola
-    Date 2024
-    license: public Domain
-*/
+// Comment block here
+// Author:
+// Date:
 
-const URL = "https://api.nasa.gov/planetary/apod";
 
-// create a button listener
-$("#activate").click(function(){
-  console.log("Click");
-  // call ajax
-  $.ajax(ajaxObj);
-})
+var URL = "https://xkcd.com/info.0.json";
 
-// setup ajax object
-const ajaxObj = {
-  url: URL,
-  data: {
-    api_key: "DEMO_KEY",
-    count: 1
-  },
-  type: "GET",
-  dataType: "json",
-  success: ajaxSuccess,
-  error: ajaxError
+// https://xkcd.com/json.html
+// endpoint https://xkcd.com/456/info.0.json
+var URLpre = "https://xkcd.com/";
+var URLpost = "info.0.json";
+
+// Endpoint format: http://xkcd.com/614/info.0.json
+
+// a helper function to turn single and double quotes into
+// html symbols so they don't confuse html tags
+//
+function make_safe(str) {
+  return str.replace(/'/g, '&apos;').replace(/"/g, '&quot;');
 }
 
-// create ajax success callback (named)
-function ajaxSuccess(data) {
-  console.log("Data:", data);
-  // parse json
-  const randomAPOD = data[0];
-  const title = randomAPOD.title;
-  const imageURL = randomAPOD.url;
-  const description = randomAPOD.explanation;
-  const date = randomAPOD.date;
+function getComic(num) {
+  if (typeof num === 'undefined') {
+    numStr = "";
+  } else {
+    numStr = num.toString() + "/";
+  }
+  var ourURL = URLpre + numStr + URLpost;
+  console.log("Our new URL:", ourURL);
+  // get data via ajax from numbersapi
+  // Using the core $.ajax() method
+  $.ajax({
+      // The URL for the request (ENDPOINT)
+      url: ourURL,
+      // Whether this is a POST or GET request
+      type: "GET",
+  })
+  // If the request succeeds
+  .done(function(data) {
+      // console.log(data);
+      var imageUrl = data.img;
+      // we use .replace(/"/g, '\\"') after the text strings because
+      // sometimes there are single and double quotes in the text
+      // that confuses the html
+      var title = data.title;
+      console.log("orig title:", title);
+      title = make_safe(title);
+      console.log("safe title:", title);
+      var alt = data.alt;
+      console.log("orig alt:", alt);
+      alt = make_safe(alt);
+      console.log("safe alt:", alt);
+      var comicNum = data.num;
+      var html = `<div id="imageblock">
+          <h2>${title}</h2>
+          <img src="${imageUrl}" title="${alt}"><br>
+          <button id="prev">Previous</button><button id="next">Next</button>
+        </div>`
+      // console.log("My new html: \n", html);
+      $("#output").html(html);
 
-  // put stuff in output div
-  $("#output").html("<h3>" + title);
-  $("#output").append("<img src='" + imageURL + "' />")
-   $("#output").append("<p class='date'>" + date);
-  $("#output").append("<p class='description'>" + description);
-  
+      // add event listener to new prev button
+      $("#prev").click(function(){
+        getComic(comicNum - 1);
+      });
+      // add event listener to new next button
+      $("#next").click(function(){
+        getComic(comicNum + 1);
+      });
+  })
+  .fail(function(){
+    console.log("^^ Please ignore this error. It's okay.");
+    console.log("Have a okay day! :-)");
+  })
+
 }
 
-
-// create ajax error callback
-function ajaxError(request,error){
-	  console.log("Oops:", error);
-}
+// start things off
+getComic();
